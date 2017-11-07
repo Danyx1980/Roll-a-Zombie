2ccc8ffb2f859d7d140b23c76fa29380a0b7e426
Daniel <daniel.chan.valette@gmail.com>
2017-11-07 14:08:43 -0400

    Server setup
    Setting up nodejs, express and all the changes needed to host the game in Heroku.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express(); 
+
+    app.set('view engine', 'ejs'); 
+    app.use(express.static(__dirname + "/public"));
+
+    app.get("/", function(req, res){
+        res.render("index"); 
+    });
+
+    app.get("*", function(req, res){
+        res.redirect("/"); 
+    })
+
+    //app.listen(3000);
+
+    app.listen(process.env.PORT, process.env.IP, function(){
+        console.log("Roll A Zombie Started!");
+    });
